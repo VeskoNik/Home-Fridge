889d3fd452a1f0723c150c3e6b474a4e40e2fe17
NikolovVesko <vcvc@mail.bg>
2023-08-04 14:49:56 +0300

    fixing errors adding css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { NavigationComponent } from './layout/header/navigation/navigation.component';
-import { CookieService } from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service'; // Make sure ngx-cookie-service is installed.
+
 import { FollowUsComponent } from './layout/footer/follow-us/follow-us.component';
 import { OurPagesComponent } from './layout/footer/our-pages/our-pages.component';
 import { PartnersComponent } from './layout/footer/partners/partners.component';
@@ -29,10 +29,11 @@ import { TestimonialsComponent } from './home/testimonials/testimonials.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CookieService
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CookieService // Add CookieService to the providers array.
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
