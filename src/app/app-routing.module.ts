1885cd7e1f33284933b01755820b8210681b1e32
NikolovVesko <vcvc@mail.bg>
2023-08-10 22:55:23 +0300

    css update and navigation update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }, 
   { path: 'create', component: CreateComponent },
